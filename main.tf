214a678a7a29f4b36eef0cc1f96b051ad3bbbcdf
kamranch24 <kamran.ch24@gmail.com>
2022-08-03 02:32:48 +0200

    added aws provider

---
+++ main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.24.0"
+    }
+  }
+}
+
+provider "aws" {
+    region = "eu-central-1"
+}
+
+
+resource "aws_s3_bucket" "cicd" {
+  bucket = "my-first-cicd-bucket"
+
+  tags = {
+    Name        = "CI/CD Bucket"
+    Environment = "Dev"
+  }
+}
