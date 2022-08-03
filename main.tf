2b5fd0d7ffd3d7fbec64746605be1d4665c701d7
kamranch24 <kamran.ch24@gmail.com>
2022-08-03 18:38:54 +0200

    aws iam group

--- main.tf
+++ main.tf
@@ -25,9 +25,28 @@ resource "aws_iam_user" "lb" {
   }
 }
 
+
 resource "aws_iam_group" "developers" {
   name = "developers"
   path = "/"
-  assume_role_policy = "${file("policy.json")}"
 
+}
+resource "aws_iam_group_policy" "my_developer_policy" {
+  name  = "my_developer_policy"
+  group = aws_iam_group.developers.name
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "ec2:Describe*",
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+    ]
+  })
 }
